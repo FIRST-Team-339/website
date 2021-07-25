05647f40dec9023a8f16525d181f6efce7acf79c
F1sxher <72705185+F1sxher@users.noreply.github.com>
2021-07-25 09:04:59 -0400

    Update Get Involved Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,6 @@ export default function Home() {
             Who's Here? <a role="button" onClick={() => scrollIntoView('description')}>Kilroy is here!</a>
           </h1>
           <br></br>
-          <br></br>
           <Image src={thumbnail} alt="Kilroy" className={styles.image} width="1013" height="582"/>
           <div className={styles.description} id="description">
             <h1><a> About Kilroy </a></h1>
@@ -37,7 +36,9 @@ export default function Home() {
             <h1><a> Get Involved </a></h1>
             <br></br>
             <div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada fringilla dapibus. Fusce purus diam, vestibulum non dui sit amet, sollicitudin ultricies dolor. Mauris tempus, odio finibus placerat porttitor, leo arcu tristique mi, id dictum lacus lorem sit amet metus. Aliquam vel felis nulla. Aliquam erat volutpat. Phasellus accumsan accumsan mi, sed vehicula quam fermentum pretium. Mauris ex metus, placerat ac consequat non, venenatis condimentum odio. Vivamus semper odio ut dictum consequat. Phasellus ullamcorper, nulla in semper suscipit, ligula mauris tempor augue, eget dignissim quam enim non quam. Pellentesque at vulputate mi. Nulla pellentesque massa eget ipsum commodo, ut aliquam justo accumsan. Suspendisse potenti. Maecenas in sagittis nunc, nec dictum enim. Nam ante felis, ultricies ac magna quis, dictum scelerisque purus.</p>
+              <p>Anyone interested in joining the team must be a high school student in Stafford, Spotsylvania, or King George County. Please <a role="button" href="/contact">Contact Us</a> to express your interest. </p>
+              <p>Mentors and volunteers can assist the team in a variety of roles. Please <a role="button" href="/contact">Contact Us</a> to express your interest.</p>
+              <p>To provide financial support or food, please visit our <a role="button" href="/donate">Donate Page</a> for more information.</p>
             </div>
           </div>
         </main>

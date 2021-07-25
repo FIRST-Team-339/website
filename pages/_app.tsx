e7170f44c4aee63e143d71cac54d39eee20a72cf
F1sxher <72705185+F1sxher@users.noreply.github.com>
2021-07-25 08:39:42 -0400

    Add cloudflare css boostrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="Who's here? Kilroy is here!! We are FIRST Robotics Team 339, located at North Stafford High School, and organized throught the Commonwealth Governor’s School."/>
         <link rel="icon" href="/favicon.ico" />
         <script src="https://kit.fontawesome.com/3a5e0f4177.js" crossOrigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css"></link>
       </Head>
       <Component {...pageProps} />
     </Layout>

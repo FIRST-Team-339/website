4f5b06983717b693fb2c48a962011ed1416caaa3
F1sxher <72705185+F1sxher@users.noreply.github.com>
2021-07-25 08:38:32 -0400

    Add copyright icon

--- Components/Footer.component.tsx
+++ Components/Footer.component.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { config } from "@fortawesome/fontawesome-svg-core";
-import {  } from "@fortawesome/free-solid-svg-icons";
+import { faCopyright } from "@fortawesome/free-solid-svg-icons";
 import { faFacebook, faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
 config.autoAddCss = false;
 
@@ -16,8 +16,6 @@ const SocialSection = styled.div`
   justify-content: center;
   align-items: center;
   width: 100%;
-  position: absolute;
-  top: 147%;
 `;
 
 const FooterSection = styled.div`
@@ -28,8 +26,6 @@ const FooterSection = styled.div`
   justify-content: center;
   align-items: center;
   width: 100%;
-  position: absolute;
-  top: 150%;
 `;
 
 const Footer = () => {
@@ -39,7 +35,7 @@ const Footer = () => {
         <h3><a role="button" href="https://twitter.com/kilroyrobotics"><FontAwesomeIcon icon={faTwitter} size="xs"/></a> <a role="button" href="https://facebook.com/kilroyrobotics"><FontAwesomeIcon icon={faFacebook} size="xs"/></a> <a role="button" href="https://github.com/FIRST-TEAM-339"><FontAwesomeIcon icon={faGithub} size="xs"/></a></h3>
       </SocialSection>
       <FooterSection>
-        <p>FIRST Team 339 | 2021 All Rights Reserved</p>
+        <p>FIRST Team 339 | <FontAwesomeIcon icon={faCopyright} size="xs"/> 2021 All Rights Reserved</p>
       </FooterSection>
     </div>
   );

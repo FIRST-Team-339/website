2b03ea0adf4e634bb0f85019fb743c5e9b14346e
F1sxher <72705185+F1sxher@users.noreply.github.com>
2021-07-26 12:05:56 -0400

    Adjusted to be easier to read, works on smaller screens

--- Components/Navbar.component.tsx
+++ Components/Navbar.component.tsx
@@ -10,7 +10,7 @@ config.autoAddCss = false;
 
 
 const Nav = styled.nav`
-  height: 80px;
+  height: auto;
   background: #000;
   display: flex;
   justify-content: space-between;
@@ -18,17 +18,14 @@ const Nav = styled.nav`
   color: #fff;
 `;
 
-const StyledLink = styled.a`
-  padding: 0rem 2rem;
-`;
-
 const Navbar = () => {
   return (
     <Nav className="navbar navbar-dark navbar-expand-md">
-      <div className="container-fluid"><a className="navbar-brand" href="/"><strong>Kilroy Robotics</strong></a><button data-toggle="collapse" className="navbar-toggler" data-target="#navcol-1"><span className="sr-only">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/"><strong>Kilroy Robotics</strong></a>
+        <button data-toggle="collapse" className="navbar-toggler" data-target="#navcol-1"><span className="sr-only">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
         <div className="collapse navbar-collapse" id="navcol-1">
           <ul className="navbar-nav">
-            <li><br></br></li>
             <li className="nav-item"><a className="nav-link" href="/" id="navlist">Home</a></li>
             <li className="nav-item"><a className="nav-link" href="/history" id="navlist">History</a></li>
             <li className="nav-item"><a className="nav-link" href="https://www.thebluealliance.com/team/339" target="_blank" rel="noopener noreferrer" id="navlist">Blue Alliance <FontAwesomeIcon icon={faExternalLinkAlt} size="xs"/></a></li>

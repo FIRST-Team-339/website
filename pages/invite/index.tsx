6b85e6b15533c0c731df2e85137848cfaac74eaf
F1sxher <me@fisherdev.tk>
2021-09-29 15:21:55 -0400

    fix[invite]: Move invite page to be index page of /invite to fix some header issues :>

--- pages/invite/index.tsx
+++ pages/invite/index.tsx
@@ -6,7 +6,7 @@ export const getServerSideProps = async (context: { res: any }) => {
   const { res } = context;
   res.writeHead(301, {
     location:
-      "https://discord.com/api/oauth2/authorize?client_id=862392047461072946&permissions=1040182527&redirect_uri=https%3A%2F%2Ftyra.digital%2Fwelcome&scope=bot%20applications.commands",
+      "https://discord.com/api/oauth2/authorize?client_id=862392047461072946&permissions=1010822398&redirect_uri=https%3A%2F%2Ftyra.digital%2Fwelcome&scope=bot%20applications.commands",
   });
   res.end();
 };

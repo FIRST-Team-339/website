ad6c00a1dbe45ef902ba241a81853febe54b840d
F1sxher <me@fisherdev.tk>
2021-11-15 16:34:03 -0500

    updates

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   // replace [yourprojectname] and [yourdomain.com] with your actual project name and (custom) domain
   cloudflareImageLoader: ({ src, width, quality }) => {
     if (!quality) {
-      quality = 75;;
+      quality = 75;
     }
-    return `https://imgs_tyra.fisherdev.workers.dev?width=${width}&quality=${quality}&image=https://dev.tyra.digital${src}`;;
-  },,
+    return `https://imgs_tyra.fisherdev.workers.dev?width=${width}&quality=${quality}&image=https://dev.tyra.digital${src}`;
+  },
 };
-;

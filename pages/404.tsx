640c7dea24e7851ba2dfec72e669afeed8e5f4e5
F1sxher <fisherjc2008@gmail.com>
2021-11-18 07:31:33 -0500

    feat[web]: Nice juicy 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Err404: NextPage = () => {
   return (
-    <div className="flex justify-center items-center w-screen h-screen bg-gray-900">
+    <div className="flex justify-center items-center w-screen vh-default-main bg-gray-900">
       <main className="bg-gray-900">
         <span className="flex justify-center items-center w-screen x1:inline text-white">
           <h1 className="text-red-400 text-8xl x1:inline font-extrabold">
@@ -18,11 +18,11 @@ const Err404: NextPage = () => {
             </span>
           </h2>
         </span>
-        <div className="mt-5 mx-auto sm:flex sm:justify-center md:mt-8 p-5 m-5">
-          <div className="rounded-md shadow flex items-center justify-center py-3 border border-transparent text-base font-medium text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10">
+        <div className="mt-5 mx-auto sm:flex sm:justify-center md:mt-8 p-5 m-10">
+          <div className="rounded-md shadow flex items-center justify-center py-3 border border-transparent text-base font-medium text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10 m-5">
             <Link href="/">Go Home</Link>
           </div>
-          <div className="rounded-md shadow flex items-center justify-center py-3 border border-transparent text-base font-medium text-blue-600 bg-white hover:bg-gray-300 md:py-4 md:text-lg md:px-10">
+          <div className="rounded-md shadow flex items-center justify-center py-3 border border-transparent text-base font-medium text-blue-600 bg-white hover:bg-gray-300 md:py-4 md:text-lg md:px-10 m-5">
             <Link href="/support">Get Support</Link>
           </div>
         </div>

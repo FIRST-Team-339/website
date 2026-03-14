5ff9957786ddab0f00beec87b8b98dbd4fdc4771
Fisher <72705185+fisherjacobc@users.noreply.github.com>
2026-03-13 23:38:18 -0400

    techbinder redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/techbinder',
+        destination: '/assets/docs/2026-Kilroy-Technical-Binder.pdf',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
